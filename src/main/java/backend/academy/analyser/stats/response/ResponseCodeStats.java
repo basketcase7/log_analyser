5dc41679233266ee24009335f13181885695bc3d
Maxim <mac28@tpu.ru>
2024-11-08 19:53:30 +0700

    feat(stats): Добавлен класс ResponseCodeStats
    Добавлен класс ResponseCodeStats, в котором содержится логика сбора статистики по часто встречающимся кодам ответа в логе.

---
+++ src/main/java/backend/academy/analyser/stats/response/ResponseCodeStats.java
@@ -0,0 +1,42 @@
+package backend.academy.analyser.stats.response;
+
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import lombok.Getter;
+
+public class ResponseCodeStats {
+
+    @Getter
+    private HashMap<Integer, Integer> responseCodesMap;
+
+    public ResponseCodeStats() {
+        responseCodesMap = new HashMap<>();
+        initResponseCodeStats();
+    }
+
+    private void initResponseCodeStats() {
+        for (Code code : Code.values()) {
+            responseCodesMap.put(code.code(), 0);
+        }
+    }
+
+    public void changeResponseCodeStats(int code) {
+        responseCodesMap.put(code, responseCodesMap.getOrDefault(code, 0) + 1);
+    }
+
+    public LinkedHashMap<Integer, Integer> sortCodeMap(HashMap<Integer, Integer> map) {
+        List<Map.Entry<Integer, Integer>> list = new LinkedList<>(map.entrySet());
+
+        list.sort(Map.Entry.comparingByValue(Comparator.reverseOrder()));
+
+        LinkedHashMap<Integer, Integer> sortedMap = new LinkedHashMap<>();
+        for (Map.Entry<Integer, Integer> entry : list) {
+            sortedMap.put(entry.getKey(), entry.getValue());
+        }
+        return sortedMap;
+    }
+}
