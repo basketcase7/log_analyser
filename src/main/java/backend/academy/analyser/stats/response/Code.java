64fa28d3675d065f618ec36aba27af22c4b2a101
Maxim <mac28@tpu.ru>
2024-11-08 19:32:37 +0700

    feat(stats): Добавлен Enum Code
    Добавлены Enum Code, в котором хранятся основные статусы http запросов с их кодом и названием.

---
+++ src/main/java/backend/academy/analyser/stats/response/Code.java
@@ -0,0 +1,36 @@
+package backend.academy.analyser.stats.response;
+
+import lombok.Getter;
+
+@Getter
+public enum Code {
+    CONTINUE(100, "Continue"),
+    SWITCHING_PROTOCOLS(101, "Switching Protocols"),
+    OK(200, "Ok"),
+    CREATED(201, "Created"),
+    ACCEPTED(202, "Accepted"),
+    NO_CONTENT(204, "No Content"),
+    MOVED_PERMANENTLY(301, "Moved Permanently"),
+    FOUND(302, "Found"),
+    SEE_OTHER(303, "See Other"),
+    NOT_MODIFIED(304, "Not Modified"),
+    BAD_REQUEST(400, "Bad Request"),
+    UNAUTHORIZED(401, "Unauthorized"),
+    FORBIDDEN(403, "Forbidden"),
+    NOT_FOUND(404, "Not Found"),
+    REQUEST_TIMEOUT(408, "Request Timeout"),
+    INTERNAL_SERVER_ERROR(500, "Internal Server Error"),
+    NOT_IMPLEMENTED(501, "Not Implemented"),
+    BAD_GATEWAY(502, "Bad Gateway"),
+    SERVICE_UNAVAILABLE(503, "Service Unavailable"),
+    GATEWAY_TIMEOUT(504, "Gateway Timeout"),;
+
+    private final int code;
+    private final String description;
+
+    Code(int code, String description) {
+        this.code = code;
+        this.description = description;
+    }
+
+}
