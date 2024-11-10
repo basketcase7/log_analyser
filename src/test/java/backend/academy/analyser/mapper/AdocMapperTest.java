8d7bdde3918560a9f70c5f63b2a468b0f4748241
Maxim <mac28@tpu.ru>
2024-11-10 18:33:14 +0700

    test(mapper): Добавлены тесты для AdocMapper
    Добавлены тесты для AdocMapper, которые проверяют работоспособность преобразования статистики в строку для adoc файла.

---
+++ src/test/java/backend/academy/analyser/mapper/AdocMapperTest.java
@@ -0,0 +1,132 @@
+package backend.academy.analyser.mapper;
+
+import backend.academy.analyser.stats.StatsHandler;
+import backend.academy.analyser.stats.date.DateStats;
+import backend.academy.analyser.stats.request.RequestStats;
+import backend.academy.analyser.stats.response.ResponseCodeStats;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import static backend.academy.analyser.mapper.AdocMapper.mapStatsToAdocString;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.anyMap;
+import static org.mockito.Mockito.when;
+
+@DisplayName("Тест класса, преобразующего статистику в строку для adoc файла")
+@ExtendWith(MockitoExtension.class)
+public class AdocMapperTest {
+    @Mock
+    private StatsHandler statsHandler;
+
+    @Mock
+    private RequestStats requestStats;
+
+    @Mock
+    private DateStats dateStats;
+
+    private String expectedAdocString = "==== Общая информация\n" +
+        "\n" +
+        "|===\n" +
+        "| Метрика | Значение\n" +
+        "| Источник логов | `C:\\Users\\Maxim\\Desktop\\backend_academy\\backend_academy_2024_project_3-java-basketcase7\\src\\main\\resources\\logs\\*short.txt`\n" +
+        "| Начальная дата | -\n" +
+        "| Конечная дата | -\n" +
+        "| Количество запросов | 11\n" +
+        "| Средний размер ответа | 451b\n" +
+        "| 95p размера ответа | 1903b\n" +
+        "|===\n" +
+        "\n" +
+        "==== Самые запрашиваемые ресурсы\n" +
+        "\n" +
+        "|===\n" +
+        "| Ресурс | Количество\n" +
+        "| `/downloads/product_1` | 8\n" +
+        "| `/downloads/product_2` | 3\n" +
+        "|===\n" +
+        "\n" +
+        "==== Самые запрашиваемые методы\n" +
+        "\n" +
+        "|===\n" +
+        "| Метод | Количество\n" +
+        "| `GET` | 11\n" +
+        "|===\n" +
+        "\n" +
+        "==== Коды ответа\n" +
+        "\n" +
+        "|===\n" +
+        "| Код | Имя | Количество\n" +
+        "| 109 | Unknown              | 5\n" +
+        "| 304 | Not Modified         | 3\n" +
+        "| 404 | Not Found            | 2\n" +
+        "|===\n" +
+        "\n" +
+        "==== Самое активное время\n" +
+        "\n" +
+        "|===\n" +
+        "| Час | Количество\n" +
+        "| `8` | 11\n" +
+        "|===\n";
+
+    @BeforeEach
+    public void setUp() {
+
+        when(statsHandler.files()).thenReturn(
+            "C:\\Users\\Maxim\\Desktop\\backend_academy\\backend_academy_2024_project_3-java-basketcase7\\src\\main\\resources\\logs\\*short.txt");
+        when(statsHandler.fromTime()).thenReturn(null);
+        when(statsHandler.toTime()).thenReturn(null);
+        when(statsHandler.requestCount()).thenReturn(11);
+        when(statsHandler.percentile()).thenReturn(1903);
+        when(statsHandler.avgResponseSize()).thenReturn(451);
+
+        when(statsHandler.requestStats()).thenReturn(requestStats);
+        LinkedHashMap<String, Integer> resourceCounts = new LinkedHashMap<>();
+        resourceCounts.put("/downloads/product_1", 8);
+        resourceCounts.put("/downloads/product_2", 3);
+        when(requestStats.requestResourceCounts()).thenReturn(resourceCounts);
+
+        LinkedHashMap<String, Integer> methodCounts = new LinkedHashMap<>();
+        methodCounts.put("GET", 11);
+        when(requestStats.requestMethodsCounts()).thenReturn(methodCounts);
+
+        ResponseCodeStats responseCodeStats = new ResponseCodeStats();
+        HashMap<Integer, Integer> codeCount = new HashMap<>();
+        codeCount.put(109, 5);
+        codeCount.put(304, 3);
+        codeCount.put(404, 2);
+        responseCodeStats.responseCodesMap(codeCount);
+        when(statsHandler.responseCodeStats()).thenReturn(responseCodeStats);
+
+        when(statsHandler.dateStats()).thenReturn(dateStats);
+        LinkedHashMap<String, Integer> hourRequestCount = new LinkedHashMap<>();
+        hourRequestCount.put("8", 11);
+        when(dateStats.hoursRequestCounts()).thenReturn(hourRequestCount);
+
+        when(statsHandler.sortRequestMap(anyMap())).thenAnswer(invocation -> {
+            Map<String, Integer> argument = invocation.getArgument(0);
+            if (argument.containsKey("/downloads/product_1")) {
+                return resourceCounts;
+            } else if (argument.containsKey("GET")) {
+                return methodCounts;
+            } else if (argument.containsKey("8")) {
+                return hourRequestCount;
+            } else {
+                return new LinkedHashMap<>();
+            }
+        });
+    }
+
+    @DisplayName("Проверка преобразования статистики в строку для adoc файла")
+    @Test
+    void testMapStatsToMarkdownString() {
+
+        String actualString = mapStatsToAdocString(statsHandler);
+
+        assertEquals(expectedAdocString, actualString);
+    }
+}
