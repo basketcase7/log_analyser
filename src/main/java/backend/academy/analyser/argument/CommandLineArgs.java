40966e09efbd644e110968220b625b8211040ec8
Maxim <mac28@tpu.ru>
2024-11-10 19:35:06 +0700

    feat(args): Добавлен класс CommandLineArgs
    Добавлен класс CommandLineArgs, который содержит валидацию введенных аргументов.

---
+++ src/main/java/backend/academy/analyser/argument/CommandLineArgs.java
@@ -0,0 +1,67 @@
+package backend.academy.analyser.argument;
+
+import com.beust.jcommander.IParameterValidator;
+import com.beust.jcommander.IStringConverter;
+import com.beust.jcommander.Parameter;
+import com.beust.jcommander.ParameterException;
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeParseException;
+import lombok.Getter;
+
+@Getter
+public class CommandLineArgs {
+    @Parameter(names = "--path", validateWith = ValidatorPath.class, required = true)
+    private String filePath;
+    @Parameter(names = "--from", converter = DateConverter.class)
+    private LocalDateTime fromTime;
+    @Parameter(names = "--to", converter = DateConverter.class)
+    private LocalDateTime toTime;
+    @Parameter(names = "--format", validateWith = ValidatorFormat.class)
+    private String outFormat;
+    @Parameter(names = "--filter-field")
+    private String filterField;
+    @Parameter(names = {"--filter-value"})
+    private String filterValue;
+
+    public static class ValidatorPath implements IParameterValidator {
+
+        @Override
+        public void validate(String name, String value) throws ParameterException {
+            if (!value.contains("\\") && !value.contains(".") && !value.contains("http://")
+                && !value.contains("https://") && !value.contains("ftp://")) {
+                throw new ParameterException("Invalid path: " + value);
+            }
+        }
+    }
+
+    public static class ValidatorFormat implements IParameterValidator {
+
+        @Override
+        public void validate(String name, String value) throws ParameterException {
+            if (!formatIsValid(value)) {
+                throw new ParameterException("Unknown format: " + value);
+            }
+        }
+
+        private boolean formatIsValid(String formatString) {
+            return "markdown".equals(formatString) || "adoc".equals(formatString);
+        }
+    }
+
+    public static class DateConverter implements IStringConverter<LocalDateTime> {
+        @Override
+        public LocalDateTime convert(String isoDateTime) {
+            try {
+                ZonedDateTime zonedDateTime = ZonedDateTime.parse(isoDateTime);
+                return zonedDateTime.toLocalDateTime();
+            } catch (DateTimeParseException e) {
+                DateTimeParseException newException =
+                    new DateTimeParseException("Error with parse date" + e.getMessage(), isoDateTime, 0);
+                newException.initCause(e);
+                throw newException;
+            }
+        }
+    }
+
+}
