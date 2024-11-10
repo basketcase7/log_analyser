67cd6fde6dd90c860644677b80fc52d70da19f5b
Maxim <mac28@tpu.ru>
2024-11-10 20:25:21 +0700

    fix(analyser): Изменен класс Main для начала работы программы

--- src/main/java/backend/academy/Main.java
+++ src/main/java/backend/academy/Main.java
@@ -1,9 +1,13 @@
 package backend.academy;
 
+import backend.academy.analyser.LogAnalyserHandler;
 import lombok.experimental.UtilityClass;
 
 @UtilityClass
 public class Main {
     public static void main(String[] args) {
+
+        LogAnalyserHandler logAnalyserHandler = new LogAnalyserHandler(args);
+        logAnalyserHandler.handle();
     }
 }
